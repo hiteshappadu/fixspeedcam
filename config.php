b427349ffc8b1a838633e75ad049b39989e7f54f
hiteshappadu <64296382+hiteshappadu@users.noreply.github.com>
2021-02-28 07:00:09 +0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$server     = 'dashdb-txn-sbox-yp-lon02-04.services.eu-gb.bluemix.net';
+$username   = 'qtw60742';
+$password   = 'v5z7848d9pp+b1fg';
+$database   = 'BLUDB';
+$port       = '5000';
+
+$dsn        = "mysql:host=$server;dbname=$database;port=$port;";
+
+?>
